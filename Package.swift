da06a5807026bcbc025d11536fd586d02c0c3801
winrid <winrid@gmail.com>
2026-04-02 20:57:23 -0700

    bump swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "FastCommentsUI", targets: ["FastCommentsUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/FastComments/fastcomments-swift.git", exact: "1.2.0")
+        .package(url: "https://github.com/FastComments/fastcomments-swift.git", exact: "1.3.0")
     ],
     targets: [
         .target(
